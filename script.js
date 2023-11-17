e64f0d93da4cbc69aa4a908c8d4666df009a893e
Salman Hossain Saif <epbd.saif@gmail.com>
2023-11-17 23:47:59 +0600

    css update

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let counted = document.getElementById("countStr")
 let count = 0
 // add
 function addition() {
-    count +=1
+    count += 1
     countStr.textContent = count
 }
 // sub
 
 function subtrack() {
-    count -=1
+    count -= 1
     countStr.textContent = count
 }
 
