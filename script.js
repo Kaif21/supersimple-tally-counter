b459ba3ae7d2c60975641a48e9fda815bb888107
Salman Hossain Saif <epbd.saif@gmail.com>
2023-11-18 00:58:01 +0600

    Update counter list item to show count and add a delete button

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function updateCounterList() {
     counters.forEach((counter, index) => {
         const listItem = document.createElement("li")
         listItem.classList.add('flex', 'justify-between', 'border-b', 'border-stone-700');
-        listItem.innerHTML = `<a href="?id=counter-${index + 1}" onclick="loadCounter(${index})" class="block w-full px-4 py-4  cursor-pointer focus:outline-none focus:ring-2 border-gray-600 hover:bg-gray-800 hover:text-white focus:ring-gray-500 focus:text-white">${counter.id}</a>
+        listItem.innerHTML = `<a href="?id=counter-${index + 1}" onclick="loadCounter(${index})" class="block w-full px-4 py-4  cursor-pointer focus:outline-none focus:ring-2 border-gray-600 hover:bg-gray-800 hover:text-white focus:ring-gray-500 focus:text-white">${counter.id} <span class="bg-stone-900 rounded-lg px-2 py-1 font-bold ml-2"> ${counter.count}</span></a>
         
         <button class=" text-red-500 px-6 border-l border-stone-700" onclick="deleteCounter(${index})"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
