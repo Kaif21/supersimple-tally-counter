4a92a7cdeb93d6e8c36aa83c36e96762439c867e
Salman Hossain Saif <epbd.saif@gmail.com>
2023-11-18 11:06:22 +0600

    Update frontend code and assets

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,18 @@
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-  );
+// This is the "Offline copy of pages" service worker
 
-  workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image' ,
-    new workbox.strategies.CacheFirst()
-  )
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
